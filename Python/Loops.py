b410c29a3908ab8e90a65667ec07e156588f544a
Shubham Das <64020241+S-ctrl172@users.noreply.github.com>
2024-09-29 00:30:03 +0530

    Update Loops.py

--- Python/Loops.py
+++ Python/Loops.py
@@ -1,4 +1,5 @@
-n = int(input())
+if __name__ == '__main__':
+    n = int(input())  # Read the integer input
 
-for i in range(n):
-    print i ** 2
+    for i in range(n):  # Loop through the range
+        print(i ** 2)   # Print the square of i
